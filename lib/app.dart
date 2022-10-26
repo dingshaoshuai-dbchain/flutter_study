e0c9cc3b86d1d0536ad083aa7bae49bbfdfb2edb
Shaoshuai Ding <xdingshaoshuai@163.com>
2022-10-26 17:58:28 +0800

    添加屏幕适配

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oktoast/oktoast.dart';
 
 import 'main_page.dart';
@@ -17,13 +18,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeProvider().getTheme(),
       home: const MainPage(),
     );
+
     /// Toast 配置
-    return OKToast(
+    Widget toast = OKToast(
         backgroundColor: Colors.black54,
         textPadding:
-        const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
         radius: 20.0,
         position: ToastPosition.bottom,
         child: app);
+
+    /// 屏幕适配
+    Widget screen = ScreenUtilInit(
+      /// 设计图尺寸
+      designSize: const Size(375, 667),
+
+      /// 是否根据宽度/高度中的最小值适配文字
+      minTextAdapt: true,
+
+      /// 支持分屏尺寸
+      splitScreenMode: true,
+      builder: (_, __) {
+        return toast;
+      },
+    );
+    return screen;
   }
 }
