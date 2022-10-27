92325870c87bd3a0f5af81f1d95939a59fdeb51b
Shaoshuai Ding <xdingshaoshuai@163.com>
2022-10-27 15:18:31 +0800

    ..

--- lib/page/my/my_index_page.dart
+++ lib/page/my/my_index_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youliao/util/toast_util.dart';
 import 'package:youliao/widgets/basis/button_widget.dart';
-import 'package:youliao/widgets/gaps.dart';
+import 'package:youliao/widgets_app/AppButton.dart';
 
 class MyIndexPage extends StatefulWidget {
   const MyIndexPage({super.key});
@@ -43,17 +44,17 @@ class _MyIndexPageState extends State<MyIndexPage> {
           textColor: Colors.yellow,
           fontSize: 15,
           marginTop: 20,
-          marginLeft: 20,
-          marginRight: 20,
+          marginLeft: 20.w,
           height: 100,
           backgroundColor: Colors.blue,
           radius: 30,
-          borderSide: BorderSide(
-            color: Colors.yellow,
-            width: 5
-          ),
+          borderSide: BorderSide(color: Colors.yellow, width: 5),
+          onPressed: () => {Toast.show('点击')},
+        ),
+        AppButton(
+          text: '登录',
           onPressed: ()=>{
-            Toast.show('点击')
+            Toast.show("登录")
           },
         )
       ],
---
+++ lib/widgets_app/AppButton.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:youliao/res_app/app_colors.dart';
+import 'package:youliao/widgets/basis/button_widget.dart';
+
+class AppButton extends StatelessWidget {
+  const AppButton({
+    super.key,
+    required this.text,
+    this.margin,
+    this.marginHorizontal = 28,
+    this.marginVertical,
+    this.marginLeft,
+    this.marginTop,
+    this.marginRight,
+    this.marginBottom,
+    this.onPressed,
+  });
+
+  final String text;
+
+  final double? margin;
+  final double? marginHorizontal;
+  final double? marginVertical;
+  final double? marginTop;
+  final double? marginBottom;
+  final double? marginLeft;
+  final double? marginRight;
+
+  final GestureTapCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonWidget(
+      text: text,
+      textColor: Colors.white,
+      fontSize: 19.sp,
+      fontWeight: FontWeight.w600,
+      width: 1.sw,
+      height: 45.w,
+      backgroundColor: AppColors.main,
+      radius: 22.5.w,
+      margin: (margin)?.w,
+      marginHorizontal: (marginHorizontal)?.w,
+      marginVertical: (marginVertical)?.w,
+      marginTop: (marginTop)?.w,
+      marginBottom: (marginBottom)?.w,
+      marginLeft: (marginLeft)?.w,
+      marginRight: (marginRight)?.w,
+      onPressed: onPressed,
+    );
+  }
+}
